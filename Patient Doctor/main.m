fb46dcc6b79437b76223e4b9f0b8aa95aaa1e85f
Samya <samya89@gmail.com>
2015-03-12 11:03:00 -0700

    Initial Commit

---
+++ Patient Doctor/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Patient Doctor
+//
+//  Created by Samia Al Rahmani on 3/12/15.
+//  Copyright (c) 2015 Samia Al Rahmani. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
